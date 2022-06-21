a101c562fba2ab3cc308d686e0dfd4550cfdf153
SerjDrob <serjdrob@gmail.com>
2022-06-21 11:40:44 +0700

    ExtParamsAdapter added

--- BehaviourTree/FuncTree.cs
+++ BehaviourTree/FuncTree.cs
@@ -4,48 +4,6 @@ using System.Threading.Tasks;
 
 namespace MachineClassLibrary.BehaviourTree
 {
-
-    //public class ActionTree
-    //{
-    //    private readonly Action _myAction;
-    //    public void DoAction () => _myAction.Invoke();
-    //    public Action GetAction() => _myAction;
-    //    protected ActionTree(Action action)
-    //    {
-    //        _myAction = action;
-    //    }
-    //    public static ActionTree SetAction(Action action) => new ActionTree(action);
-    //    public static MakeLoop StartLoop(int count) => new MakeLoop(count);
-    //    public class MakeLoop
-    //    {
-    //        public MakeLoop(int count)
-    //        {
-    //            _count = count;
-    //        }
-    //        private List<ActionTree> _children = new();
-    //        private readonly int _count;
-
-    //        public MakeLoop AddChild(ActionTree child)
-    //        {
-    //            _children.Add(child);
-    //            return this;
-    //        }
-    //        public ActionTree EndLoop
-    //        {
-    //            get => new ActionTree(() =>
-    //            {
-    //                for (int i = 0; i < _count; i++)
-    //                {
-    //                    foreach (var item in _children)
-    //                    {
-    //                        item.DoAction();
-    //                    }
-    //                }
-    //            });
-    //        }
-    //    }
-    //}
-
     public class FuncTree
     {
         private readonly Func<Task> _myTask;
--- Laser/ExtendedParams.cs
+++ Laser/ExtendedParams.cs
@@ -65,31 +65,4 @@ namespace MachineClassLibrary.Laser
         [DisplayName("Шаг")]
         public int HatchLineDistance { get; set; }
     }
-
-    public class ExtParamsAdapter
-    {
-        private readonly ExtendedParams _extendedParams;
-
-        public ExtParamsAdapter(ExtendedParams extendedParams)
-        {
-            _extendedParams = extendedParams;
-        }
-        public MarkLaserParams MixParams(MarkLaserParams markLaserParams)
-        {
-            var penParams = markLaserParams.PenParams with
-            {
-                MarkLoop = _extendedParams.MarkLoop,
-                MarkSpeed = _extendedParams.MarkSpeed,
-                PowerRatio = _extendedParams.PowerRatio,
-                Freq = _extendedParams.Freq,
-                QPulseWidth = _extendedParams.QPulseWidth
-            };
-            var hatchParams = markLaserParams.HatchParams with
-            {
-                HatchLineDist = _extendedParams.HatchLineDistance
-            };
-
-            return new MarkLaserParams(penParams, hatchParams);
-        }
-    }
 }
