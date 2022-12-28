2875ddb1ce194ca55de3adc200c0361168004bbb
Сергей Дробышев <serjdrob@gmail.com>
2022-12-28 16:30:02 +0700

    Added AxisStateView

---
+++ Machine/MachineAxesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MachineClassLibrary.Machine;
+
+namespace MachineClassLibrary.Machine
+{
+    public class MachineAxesConfiguration
+    {
+        public string LineComment { get => "Line coefficient for coordinates equiped with encoder. Can be negative. Set 0 if no encoder"; }
+        public double XLine { get; set; }
+        public double YLine { get; set; }
+        public double ZLine { get; set; }
+        public double ULine { get; set; }
+        public string RightDirectionComment { get => "Set true if moving right is the positive direction"; }
+        public bool XRightDirection { get; set; }
+        public bool YRightDirection { get; set; }
+        public bool ZRightDirection { get; set; }
+        public bool URightDirection { get; set; }
+        public string ZToObjAproxComment { get => "Set true if z move toward the objective in the positive direction"; }
+        public bool ZToObjectiveAproximation { get; set; }
+
+    }
+}
