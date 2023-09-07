61d7d83ea865ae7c64827062eaaa490b60c03b8f
Сергей Дробышев <serjdrob@gmail.com>
2023-09-07 10:34:43 +0700

    Added ParamsProfile

---
+++ Laser/Parameters/ParamsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace MachineClassLibrary.Laser.Parameters;
+public class ParamsProfile : Profile
+{
+    public ParamsProfile()
+    {
+        CreateMap<MarkLaserParams, ExtendedParams>()
+            .IncludeMembers(s => s.PenParams, s => s.HatchParams);
+
+        CreateMap<PenParams, ExtendedParams>(MemberList.None);
+        CreateMap<HatchParams, ExtendedParams>(MemberList.None);
+    }
+}
