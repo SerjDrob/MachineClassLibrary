b67688e8bcd7dfa4a9062ff3ded672f1dc19a682
Сергей Дробышев <serjdrob@gmail.com>
2024-03-25 10:10:17 +0700

    created MotionDevicePCIE1245.cs

--- Machine/MotionDevices/MotionBoardFactory.cs
+++ Machine/MotionDevices/MotionBoardFactory.cs
@@ -17,6 +17,7 @@ namespace MachineClassLibrary.Machine.MotionDevices
         {
             if (_boardConfiguration.IsPCI1240U) return _serviceProvider.GetService<MotionDevicePCI1240U>() ?? throw new NullReferenceException("Getting service MotionDevicePCI1240U returned null");
             if (_boardConfiguration.IsPCI1245E) return _serviceProvider.GetService<MotionDevicePCI1245E>() ?? throw new NullReferenceException("Getting service MotionDevicePCI1245E returned null");
+            if (_boardConfiguration.IsPCIE1245) return _serviceProvider.GetService<MotionDevicePCIE1245>() ?? throw new NullReferenceException("Getting service MotionDevicePCI1245E returned null");
             if (_boardConfiguration.IsMOCKBOARD) return _serviceProvider.GetService<MotDevMock>() ?? throw new NullReferenceException("Getting service MotDevMock returned null");
             throw new ArgumentException($"The motion board isn't defined.");
         }
