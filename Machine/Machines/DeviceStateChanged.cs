9771a3edc66aba743cf3ef96dbb839828efd3ad0
Сергей Дробышев <serjdrob@gmail.com>
2024-04-04 15:07:42 +0700

    just ordnung

--- Machine/Machines/DeviceStateChanged.cs
+++ Machine/Machines/DeviceStateChanged.cs
@@ -1,6 +1,4 @@
 ﻿namespace MachineClassLibrary.Machine.Machines
 {
-    public interface IDeviceStateChanged { };
     public record DeviceStateChanged():IDeviceStateChanged;
-    public record SensorStateChanged(LaserSensor Sensor, bool state):IDeviceStateChanged;
 }
