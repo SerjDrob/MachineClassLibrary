39faed81191fbc7874aef2c1babad30e3af57d70
Сергей Дробышев <serjdrob@gmail.com>
2025-10-10 19:31:36 +0700

    refactor: AxisStateView

--- Machine/AxisStateView.cs
+++ Machine/AxisStateView.cs
@@ -1,6 +1,3 @@
-﻿using MachineClassLibrary.Machine;
+﻿namespace MachineClassLibrary.Machine;
 
-namespace MachineClassLibrary.Machine
-{
-    public record AxisStateView(double Position, double CmdPosition, bool NLmt, bool PLmt, bool MotionDone, bool MotionStart, bool EZ, bool ORG, int GoPositive);
-}
+public record AxisStateView(double Position, double CmdPosition, bool NLmt, bool PLmt, bool MotionDone, bool MotionStart, bool EZ, bool ORG, int GoPositive);
