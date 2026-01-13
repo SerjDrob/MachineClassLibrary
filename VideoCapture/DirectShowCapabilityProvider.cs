04d404bb738881009f35b19f5ac857c19b4a4cfb
Сергей Дробышев <serjdrob@gmail.com>
2026-01-13 18:20:31 +0700

    feat: added DirectShowCapabilityProvider

---
+++ VideoCapture/DirectShowCapabilityProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using AForge.Video.DirectShow;
+
+namespace MachineClassLibrary.VideoCapture;
+
+public sealed class DirectShowCapabilityProvider
+    : IVideoCapabilityProvider
+{
+    public IReadOnlyList<VideoCapability> GetCapabilities(int cameraIndex)
+    {
+        var devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        var device = new VideoCaptureDevice(devices[cameraIndex].MonikerString);
+
+        var list = new List<VideoCapability>();
+
+        foreach (var cap in device.VideoCapabilities)
+        {
+            list.Add(new VideoCapability(
+                cap.FrameSize.Width,
+                cap.FrameSize.Height,
+                cap.AverageFrameRate));
+        }
+
+        return list;
+    }
+}
+
