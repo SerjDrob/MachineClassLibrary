a6347391d32c2ef4cbcda60e3e77b23dc5bb7eb2
Сергей Дробышев <serjdrob@gmail.com>
2026-03-03 18:59:49 +0700

    changed

--- GeometryUtility/CoorSystem.WorkMatrixCoorSystemBuilder.cs
+++ GeometryUtility/CoorSystem.WorkMatrixCoorSystemBuilder.cs
@@ -2,35 +2,34 @@
 using System;
 using System.Numerics;
 
-namespace MachineClassLibrary.GeometryUtility
+namespace MachineClassLibrary.GeometryUtility;
+
+public partial class CoorSystem<TPlaceEnum> where TPlaceEnum : Enum
 {
-    public partial class CoorSystem<TPlaceEnum> where TPlaceEnum : Enum
+    public class WorkMatrixCoorSystemBuilder<TPlace> where TPlace : Enum
     {
-        public class WorkMatrixCoorSystemBuilder<TPlace> where TPlace : Enum
+        private Matrix3 _workMatrix;
+
+        public WorkMatrixCoorSystemBuilder<TPlace> SetWorkMatrix(Matrix3x2 workMatrix)
         {
-            private Matrix3 _workMatrix;
 
-            public WorkMatrixCoorSystemBuilder<TPlace> SetWorkMatrix(Matrix3x2 workMatrix)
-            {
-
-                _workMatrix = workMatrix.ConvertMatrix();
-                return this;
-            }
-            public WorkMatrixCoorSystemBuilder<TPlace> SetWorkMatrix(float[] elements)
-            {
-                if (elements.Length != 6) throw new ArgumentException("The count of the elements must be 6");
-                return SetWorkMatrix(new Matrix3x2(elements[0],
-                                                   elements[1],
-                                                   elements[2],
-                                                   elements[3],
-                                                   elements[4],
-                                                   elements[5]));
-            }
-            
-            public CoorSystem<TPlace> Build()
-            {
-                return new CoorSystem<TPlace>(_workMatrix);
-            }
+            _workMatrix = workMatrix.ConvertMatrix();
+            return this;
+        }
+        public WorkMatrixCoorSystemBuilder<TPlace> SetWorkMatrix(float[] elements)
+        {
+            if (elements.Length != 6) throw new ArgumentException("The count of the elements must be 6");
+            return SetWorkMatrix(new Matrix3x2(elements[0],
+                                               elements[1],
+                                               elements[2],
+                                               elements[3],
+                                               elements[4],
+                                               elements[5]));
+        }
+        
+        public CoorSystem<TPlace> Build()
+        {
+            return new CoorSystem<TPlace>(_workMatrix);
         }
     }
 }
