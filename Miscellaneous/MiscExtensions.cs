7898fed9b4a293c5b881f5a6fbab3b35d76ef56c
Сергей Дробышев <serjdrob@gmail.com>
2026-05-21 10:43:04 +0700

    fix: MiscExtensions

--- Miscellaneous/MiscExtensions.cs
+++ Miscellaneous/MiscExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
 
